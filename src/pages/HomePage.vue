937aba4adbad4d6d7ce070e4f59c0e3a2f43352b
reistence <gvlamberti@gmail.com>
2023-01-21 15:16:59 +0100

    kute js attempt - failed

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,14 +1,40 @@
 <script>
 
 export default {
-    name: "HomePage"
+    name: "HomePage",
+    data(){
+        return{
+            // blob1 : this.$refs.blob1,
+            // blob2 : this.$refs.blob2,
+           
+        }
+    },
+    mounted(){
+        // this.moveBlobs()
+    },
+    methods: {
+        // moveBlobs(){
+
+        //     blob1 = this.$refs.blob1;
+        //     blob2 = this.$refs.blob2;
+        //     console.log(blob1);
+        // const tween = KUTE.fromTo(
+        //   blob1,
+        //   { path: blob1 },
+        //   { path: blob2 },
+        //   { repeat: 999, duration: 3000, yoyo: true }
+        // ).start();}
+    }
 }
 
 </script>
 
 <template>
     <main>
-        <h1>Homepage</h1>
+        <div class="container">
+            <h1>Home</h1>
+
+        </div>
     </main>
 </template>
 
@@ -17,8 +43,22 @@ export default {
 @use "../styles/partials/_mixins" as*;
 @use "../styles/partials/_variables" as*;
 
+.container{
+    @include my-container;
+}
+
     h1{
         color: $red;
+        text-align: center;
     }
+
+    // .pink {
+    //     background: #ff0066;
+    //     overflow: hidden;
+    //   }
+
+    //   .blob-content {
+    //     z-index: 1;
+    //   }
 
 </style>
